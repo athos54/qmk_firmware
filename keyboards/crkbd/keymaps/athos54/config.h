2b85dbc61a6dba277e7ec5c0be74871f4227b420
athos <athos.oc@gmail.com>
2021-02-23 13:14:35 +0100

    add More time to autoshift

--- keyboards/crkbd/keymaps/athos54/config.h
+++ keyboards/crkbd/keymaps/athos54/config.h
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
+#define AUTO_SHIFT_TIMEOUT 200
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
